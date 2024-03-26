07727da53479d3e0af1b54cad81ca8f8802efff7
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-26 18:38:47 -0400

    Initial Commit

---
+++ MultiplicationGame/MultiplicationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationGameApp.swift
+//  MultiplicationGame
+//
+//  Created by Ahsan Qureshi on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
